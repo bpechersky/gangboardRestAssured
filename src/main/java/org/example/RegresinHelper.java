02714875862693824e678c72177302445ba66694
Bairagi Muduli <bairagi.muduli@mpokket.com>
2021-07-14 07:29:57 +0530

    added new get methods

--- src/main/java/org/example/RegresinHelper.java
+++ src/main/java/org/example/RegresinHelper.java
@@ -1,15 +1,35 @@
 package org.example;
 
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.example.BaseHelper;
+
+import java.util.Map;
 
 public class RegresinHelper {
-    public static String url = "https://reqres.in/";
+    public static String baseUrl = "https://reqres.in/";
 
     BaseHelper baseHelper = new BaseHelper();
 
-    public Response get(String pathParam){
-        Response response = baseHelper.get(url+pathParam);
+    public Response getUserList(int pageNo){
+        String pathParam = "api/users?page=";
+        String finalUrl = baseUrl +pathParam+pageNo;
+        System.out.println("final url is : "+finalUrl);
+        Response response = baseHelper.get(finalUrl);
         return response;
+    }
+
+    /**
+     * https://reqres.in/api/users/2
+     *
+     * @param userId
+     * @return
+     */
+    public Response getUSerDetails(int userId){
+        String pathParam = "api/users/";
+        String finalUrl = baseUrl+pathParam+userId;
+        System.out.println("final url is : "+finalUrl);
+        Response response = baseHelper.get(finalUrl);
+        return response;
+
     }
 }
--- src/test/java/org/example/org/example/base/regresin/TestRegressinGet.java
+++ src/test/java/org/example/org/example/base/regresin/TestRegressinGet.java
@@ -8,10 +8,22 @@ import org.testng.annotations.Test;
 public class TestRegressinGet {
     RegresinHelper regresinHelper = new RegresinHelper();
 
+    /**
+     * https://reqres.in/api/users?page=2    //user list
+     * /api/users/2 //user details
+     */
+
     @Test
-    public void testGet(){
-        Response response = regresinHelper.get("get");
+    public void testGetUserLIst(){
+        Response response = regresinHelper.getUserList(2);
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200, "code is wrong");
+    }
+
+    @Test
+    public void testGetUserDetails() {
+        Response uSerDetails = regresinHelper.getUSerDetails(2);
+        int statusCode = uSerDetails.getStatusCode();
+        Assert.assertEquals(statusCode,200, "unexpected behaviour");
     }
 }
