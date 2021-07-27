c33c5618bf850ef203fd47b454177b8635cde30a
Bairagi Muduli <bairagi.muduli@mpokket.com>
2021-07-27 08:56:33 +0530

    removed unnecessary

--- src/main/java/org/example/pojos/herokuapp/AvailablePetsResponse.java
+++ src/main/java/org/example/pojos/herokuapp/AvailablePetsResponse.java
@@ -1,16 +1,12 @@
 
 package org.example.pojos.herokuapp;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import javax.annotation.Generated;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
@@ -21,7 +17,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
     "tags",
     "status"
 })
-@Generated("jsonschema2pojo")
+@Data
 public class AvailablePetsResponse {
 
     @JsonProperty("id")
@@ -36,77 +32,4 @@ public class AvailablePetsResponse {
     private List<Tag> tags = null;
     @JsonProperty("status")
     private String status;
-    @JsonIgnore
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-    @JsonProperty("id")
-    public Long getId() {
-        return id;
-    }
-
-    @JsonProperty("id")
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @JsonProperty("category")
-    public Category getCategory() {
-        return category;
-    }
-
-    @JsonProperty("category")
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    @JsonProperty("name")
-    public String getName() {
-        return name;
-    }
-
-    @JsonProperty("name")
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @JsonProperty("photoUrls")
-    public List<String> getPhotoUrls() {
-        return photoUrls;
-    }
-
-    @JsonProperty("photoUrls")
-    public void setPhotoUrls(List<String> photoUrls) {
-        this.photoUrls = photoUrls;
-    }
-
-    @JsonProperty("tags")
-    public List<Tag> getTags() {
-        return tags;
-    }
-
-    @JsonProperty("tags")
-    public void setTags(List<Tag> tags) {
-        this.tags = tags;
-    }
-
-    @JsonProperty("status")
-    public String getStatus() {
-        return status;
-    }
-
-    @JsonProperty("status")
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
-
 }
